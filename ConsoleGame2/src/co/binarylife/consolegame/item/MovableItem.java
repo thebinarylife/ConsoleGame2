e125f9afa883955a580ab6fcbf758538f42e53d4
thebinarylife <tbl@binarylife.co>
2019-10-21 13:19:06 -0400

    Added Movable Item Interface changed from uninteractable to unmovable... changed to interfaces for heirarchy purposes

--- ConsoleGame2/src/co/binarylife/consolegame/item/MovableItem.java
+++ ConsoleGame2/src/co/binarylife/consolegame/item/MovableItem.java
@@ -1,10 +1,5 @@
 package co.binarylife.consolegame.item;
 
-public abstract class MovableItem extends Item {
-	
-	public MovableItem(int id, String name) {
-		super(id, name);
-	}
-	
+public interface MovableItem {
 	public abstract void pickup();
 }
--- ConsoleGame2/src/co/binarylife/consolegame/item/UnmovableItem.java
+++ ConsoleGame2/src/co/binarylife/consolegame/item/UnmovableItem.java
@@ -1,12 +1,5 @@
 package co.binarylife.consolegame.item;
 
 // ABSTRACT MEANS this is a SUPERCLASS where all sub items must implement their undefined methods
-public abstract class UnmovableItem extends Item {
-	
-	public UnmovableItem(int id, String name)	{
-		super(id, name);
-	}
-	
-	public abstract void interactAlert();
-	public abstract void loadCommands();
+public interface UnmovableItem {
 }
